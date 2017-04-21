1a635d3b204043a8e413eebce9897e6e79f5fa69
Randall <ragrate@gmail.com>
2017-04-21 02:07:21 +0000

    Natural date in url

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 var express = require("express")
 var app = express()
+var http = require("http")
+var url = require("url")
+var timestamp = require("unix-timestamp")
+var moment = require("moment")
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/:time', function (req, res) {
+  var time = new Date(req.params.time)
+  if(time) {
+  var unixTime = timestamp.fromDate(time).toString()
+  var naturalTime = moment(time).format("MMMM Do YYYY")
+  }
+  // } else if(timestamp.toDate(req.params.time) {
+  //   var naturalTime = moment(time).format("MMMM Do YYYY")
+  // }
+  res.send(JSON.stringify({unix: unixTime, natural: naturalTime}))
 })
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('timestamp app listening on port 8080!')
 })
